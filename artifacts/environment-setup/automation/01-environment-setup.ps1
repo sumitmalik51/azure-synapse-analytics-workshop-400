7c9dd17e3bd1647c2fafac3602bfc9b7ed7c0fac
srushti-714 <55180169+srushti-714@users.noreply.github.com>
2020-08-25 21:25:30 +0530

    Update 01-environment-setup.ps1

--- artifacts/environment-setup/automation/01-environment-setup.ps1
+++ artifacts/environment-setup/automation/01-environment-setup.ps1
@@ -9,7 +9,21 @@ if($IsCloudLabs){
         Import-Module "..\solliance-synapse-automation"
 
         . C:\LabFiles\AzureCreds.ps1
+        
+         $depId = $deploymentID
+        $initstatus = "Started"
+        $validstatus = "NotValidated"
 
+        $uri = 'https://prod-04.centralus.logic.azure.com:443/workflows/8f1e715486db4e82996e45f86d84edc6/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=5fJRgTtLIkSidgMmhFXU_DfubS837o8po0BBvCGuGeA'
+        $bodyMsg = @(
+             @{ "DeploymentId" = "$depId"; 
+              "InitiationStatus" =  "$initstatus"; 
+              "ValidationStatus" = "$validstatus" }
+              )
+       $body = ConvertTo-Json -InputObject $bodyMsg
+       $header = @{ message = "StartedByScript"}
+       $response = Invoke-RestMethod -Method post -Uri $uri -Body $body -Headers $header  -ContentType "application/json"
+       
         $userName = $AzureUserName                # READ FROM FILE
         $password = $AzurePassword                # READ FROM FILE
         $clientId = $TokenGeneratorClientId       # READ FROM FILE
@@ -685,3 +699,17 @@ Update-AzCosmosDBSqlContainer -ResourceGroupName $resourceGroupName `
         -Name $cosmosDbContainer -Throughput 400 `
         -PartitionKeyKind $container.Resource.PartitionKey.Kind `
         -PartitionKeyPath $container.Resource.PartitionKey.Paths
+        
+        $depId = $deploymentID
+        $initstatus = "Completed"
+        $validstatus = "Tobestarted"
+
+        $uri = 'https://prod-04.centralus.logic.azure.com:443/workflows/8f1e715486db4e82996e45f86d84edc6/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=5fJRgTtLIkSidgMmhFXU_DfubS837o8po0BBvCGuGeA'
+        $bodyMsg = @(
+             @{ "DeploymentId" = "$depId"; 
+              "InitiationStatus" =  "$initstatus"; 
+              "ValidationStatus" = "$validstatus" }
+              )
+       $body = ConvertTo-Json -InputObject $bodyMsg
+       $header = @{ message = "StartedByScript"}
+       $response = Invoke-RestMethod -Method post -Uri $uri -Body $body -Headers $header  -ContentType "application/json"
