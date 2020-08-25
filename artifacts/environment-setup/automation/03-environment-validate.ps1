702a81f8c830af30af094aa31a73cce1dbe1724f
srushti-714 <55180169+srushti-714@users.noreply.github.com>
2020-08-25 21:26:28 +0530

    Update 03-environment-validate.ps1

--- artifacts/environment-setup/automation/03-environment-validate.ps1
+++ artifacts/environment-setup/automation/03-environment-validate.ps1
@@ -479,4 +479,15 @@ else {
     Write-Warning "Validation Failed - see log output"
 }
 
+        $depId = $deploymentID
+        $initstatus = "Started"
 
+      $uri = 'https://prod-04.centralus.logic.azure.com:443/workflows/8f1e715486db4e82996e45f86d84edc6/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=5fJRgTtLIkSidgMmhFXU_DfubS837o8po0BBvCGuGeA'
+        $bodyMsg = @(
+             @{ "DeploymentId" = "$depId"; 
+              "InitiationStatus" =  "$initstatus"; 
+              "ValidationStatus" = "$validstatus" }
+              )
+       $body = ConvertTo-Json -InputObject $bodyMsg
+       $header = @{ message = "StartedByScript"}
+       $response = Invoke-RestMethod -Method post -Uri $uri -Body $body -Headers $header  -ContentType "application/json"
